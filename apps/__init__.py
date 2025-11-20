3f2d84546e1717221c829080f9dcc47818731309
Djq <Djq@JiaQi.com>
2025-11-20 09:55:16 +0800

    继续优化架构

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 import settings
+from apps.users.view import user_bp
 
 
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
     app.config.from_object(settings.Config)
+    app.register_blueprint(user_bp)
     return app
